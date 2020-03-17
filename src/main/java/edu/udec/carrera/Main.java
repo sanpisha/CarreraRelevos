e6e070eda3ee0e7043cfece47d97e9c1862e2cdb
sago9 <sago9@LAPTOP-V0DH3NPL>
2020-03-17 15:51:44 -0500

    se agrega la clase main

---
+++ src/main/java/edu/udec/carrera/Main.java
@@ -0,0 +1,19 @@
+package edu.udec.carrera;
+
+
+
+/**
+ * Clase main inicializa el programa
+ *
+ * @author Adriana Patricia Rojas Beltran
+ */
+public class Main {
+    /**
+     * Clase main inicializa el programa
+     * @param args matriz de argumentos de línea de comando pasados ​​a este método
+     */
+    public static void main(String[] args) {
+        Principal p = new Principal();
+        p.iniciaCarrera();
+    }
+}
